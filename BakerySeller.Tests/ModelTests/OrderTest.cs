5d41066e8aa7989f339f238bffc8550a7a439dee
delguanzon <yodelguanzon@gmail.com>
2022-12-16 16:24:36 -0800

    Add test for Order instances, create method for ID

--- BakerySeller.Tests/ModelTests/OrderTest.cs
+++ BakerySeller.Tests/ModelTests/OrderTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakerySeller.Models;
 
 namespace BakerySeller.Tests 
 {
@@ -26,6 +27,24 @@ namespace BakerySeller.Tests
             Assert.AreEqual(title, newOrder.Title);
             Assert.AreEqual(description, newOrder.Description);
             Assert.AreEqual(price, newOrder.Price);
+        }
+
+        [TestMethod]
+        public void OrderConstructor_AssignIdToOrderInstance_Int()
+        {
+            int id = 1;
+            Order newOrder = new Order("title", "description", 30);
+            Assert.AreEqual(id, newOrder.Id);
+        }
+
+        [TestMethod]
+        public void OrderConstructor_AddOrderToOrderInstances_List()
+        {
+            Order newOrder1 = new Order("order1", "description1", 30);
+            Order newOrder2 = new Order("order2", "description2", 30);
+            Order newOrder3 = new Order("order3", "description3", 30);
+            List<Order> expected = new List<Order> { newOrder1, newOrder2, newOrder3 };
+            CollectionAssert.AreEqual(expected, Order.GetAll());
         }
 
 
--- BakerySeller/Models/Order.cs
+++ BakerySeller/Models/Order.cs
@@ -11,12 +11,17 @@ namespace BakerySeller.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
+        private static List<Order> _instances = new List<Order>();
+        public int Id { get; }
 
         public Order(string title, string description, int price)
         {
             Title = title;
             Description = description;
             Price = price;
+            _instances.Add(this);
+            Id = _instances.Count;
         }
+
     }
 }
