5632d4a85a08c080f5f736fda9ce296880e65132
delguanzon <yodelguanzon@gmail.com>
2022-12-16 16:14:44 -0800

    Update OrderTests

--- BakerySeller.Tests/ModelTests/OrderTest.cs
+++ BakerySeller.Tests/ModelTests/OrderTest.cs
@@ -12,10 +12,22 @@ namespace BakerySeller.Tests
         [TestMethod]
         public void OrderConstructor_CreateInstanceOfOrder_Order()
         {
-            Order newOrder = new Order("title", "description", "price", "date");
+            Order newOrder = new Order("title", "description", 30);
             Assert.AreEqual(typeof(Order), newOrder.GetType());
         }
 
-        
+        [TestMethod]
+        public void OrderConstructor_AssignValuesToOrderTitleAndDescription_String()
+        {
+            string title = "title";
+            string description = "description";
+            int price = 30;
+            Order newOrder = new Order(title, description, price);
+            Assert.AreEqual(title, newOrder.Title);
+            Assert.AreEqual(description, newOrder.Description);
+            Assert.AreEqual(price, newOrder.Price);
+        }
+
+
     }
 }
